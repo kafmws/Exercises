a57c81eef7d17e14a56e7da66dcda33c8c2d98c2
kafmws <740721665@qq.com>
2022-04-29 22:26:00 +0800

    update

--- zztest/main.cpp
+++ zztest/main.cpp
@@ -5,80 +5,54 @@
 #include <iostream>
 #include <map>
 #include <cassert>
+#include <cmath>
 
-using namespace std;
+//std::map<int, std::string, std::greater<>> Solution::m = Solution::initMap();
 
+#include <map>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-
-private:
-    int romanValue(char c) {
-        switch (c) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-        }
-        return 0;
-    }
-
 public:
-    int romanToInt(string s) {
-        s.push_back(' ');
-        int result = romanValue(s[0]);
-        for (int i = 1; i < s.length() - 1; ++i) {
-            int cur = romanValue(s[i]);
-            if (cur < s[i + 1]) {
-                result -= cur;
-            } else result += cur;
-        }
-        return result;
-    }
-
-    static map<int, string, greater<int>> getRomanMap() {
-        static map<int, string, greater<int>> m = map<int, string, greater<int>>();
-        m[1000] = string("M");
-        m[900] = string("CM");
-        m[500] = string("D");
-        m[400] = string("CD");
-        m[100] = string("C");
-        m[90] = string("XC");
-        m[50] = string("L");
-        m[40] = string("XL");
-        m[10] = string("X");
-        m[9] = string("IX");
-        m[5] = string("V");
-        m[4] = string("IV");
-        m[1] = string("I");
-        return m;
-    }
-
-    string intToRoman(int num) {
-        string result;
-        for (auto &it: m) {
-            while (num >= it.first) {
-                num -= it.first;
-
+    bool isBipartite(std::vector<std::vector<int>> &graph) {
+        int n = graph.size();
+        std::vector<int> flags = std::vector<int>(n, 2);
+        std::queue<int> queue = std::queue<int>();
+        for (int i = 0; i < n; i++) queue.push(i);
+        while (!queue.empty()) {
+            int node = queue.front();
+            if (flags[node] == 2) flags[node] = 0;
+            queue.pop();
+            for (int i: graph[node]) {
+                if (flags[i] == 2) {
+                    flags[i] = !flags[node];
+                    queue.push(i);
+                } else if (flags[i] == flags[node]) return false;
             }
         }
+        return true;
     }
-
-    Solution() = default;
 };
+//leetcode submit region end(Prohibit modification and deletion)
 
 int main() {
-    auto solution = Solution();
-    assert(solution.romanToInt(solution.intToRoman(1994)) == "MCMXCIV");
-    assert(solution.romanToInt("MCMXC") == 1990);
-    assert(solution.romanToInt("MCM") == 1900);
-
+    Solution solution = Solution();
+    std::vector<std::vector<int>> v = std::vector<std::vector<int>>() = {
+            {1},
+            {0, 3},
+            {3},
+            {1, 2},
+    };
+    std::cout << solution.isBipartite(v);
+    return 0;
 }
