1dd055208819fa823f4d5634652b274ce5d0e405
kafmws <740721665@qq.com>
2022-09-01 15:34:41 +0800

    update

--- zztest/Solution.cpp
+++ zztest/Solution.cpp
@@ -15,7 +15,6 @@ bool chmin(_Tp &a, const _Fp &b, _Compare __comp = _Compare()) {
   return __comp(b, a) ? a = b, true : false;
 }
 #define YESNO(condition) cout << condition ? "YES\n" : "NO\n";
-
 using namespace std;
 auto tam = [] {
   std::ios::sync_with_stdio(0);
@@ -53,68 +52,18 @@ auto tam = [] {
 #endif
 }();
 
-#if __cplusplus == 201703L
-#define tst 132
-#endif
+void solve() {
 
-class Solution {
-  int dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+}
 
- public:
-  vector<vector<int>> pacificAtlantic(vector<vector<int>> &heights) {
-    int opt[201][201] = {0};  // 0 1 pacific
-    int m = heights.size(), n = heights[0].size();
-    queue<pair<int, int>> q = queue<pair<int, int>>();
-    vector<vector<int>> ret;
-    for (int j = 0; j < n; j++) q.push({0, j});
-    for (int i = 1; i < m; i++) q.push({i, 0});
-    while (!q.empty()) {
-      auto p = q.front();
-      q.pop();
-      opt[p.first][p.second] = 1;
-      for (auto &dir : dirs) {
-        int x = p.first + dir[0];
-        int y = p.second + dir[1];
-        if (x >= 0 && x < m && y >= 0 && y < n && !opt[x][y] &&
-            heights[x][y] >= heights[p.first][p.second]) {
-          q.push({x, y});
-        }
-      }
-    }
-    for (int i = 0; i < m; i++) q.push({i, n - 1});
-    for (int j = n - 2; j >= 0; j--) q.push({m - 1, j});
-    while (!q.empty()) {
-      auto p = q.front();
-      q.pop();
-      if (opt[p.first][p.second] == 1) {
-        ret.emplace_back(vector<int>{p.first, p.second});
-      }
-      opt[p.first][p.second] = 2;
-      for (auto dir : dirs) {
-        int x = p.first + dir[0];
-        int y = p.second + dir[1];
-        if (x >= 0 && x < m && y >= 0 && y < n && opt[x][y] != 2 &&
-            heights[x][y] >= heights[p.first][p.second]) {
-          q.push({x, y});
-        }
-      }
-    }
-    return ret;
-  }
-
-  Solution() {}
-};
 
 int main() {
-  Solution s;
-  vector<vector<int>> v{{1, 2, 2, 3, 5},
-                        {3, 2, 3, 4, 4},
-                        {2, 4, 5, 3, 1},
-                        {6, 7, 1, 4, 5},
-                        {5, 1, 1, 2, 4}};
-  auto ret = s.pacificAtlantic(v);
-  for (auto &r : ret) cout << '[' << r[0] << ',' << r[1] << ']' << ',';
-  cout << "\b " << endl;
+  int t;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  
 
   CHECKTIME;
 }
